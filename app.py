f24a7615972281c0ed4293b34c2ad23b6e191c1d
Omar Alabed <viewvision@mac.mynetworksettings.com>
2026-01-17 03:31:14 -0500

    Add allow_unsafe_werkzeug=True for Flask-SocketIO

--- app.py
+++ app.py
@@ -486,4 +486,4 @@ if __name__ == '__main__':
     
     # Run Flask app with SocketIO
     logger.info("Starting web server on http://0.0.0.0:5000")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
